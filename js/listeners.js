84bb3703d9a1cfa309cd886a761cc519b8e3c3a4
iva_xela <varanous@gmail.com>
2022-12-05 02:43:09 +0300

    refactor: add necessary resources for "timetable"

--- js/listeners.js
+++ js/listeners.js
@@ -2,3 +2,14 @@ window.addEventListener('load', () => loadingTime());
 
 document.addEventListener('DOMContentLoaded',
     () => handleLocationChange(document.location.href));
+
+document.addEventListener('DOMContentLoaded', () => setUpSubmitHandlers())
+
+document.addEventListener('DOMContentLoaded', () => setUpActionHandlers())
+
+document.addEventListener('DOMContentLoaded', () =>
+    uploadTable(
+        document.getElementById('dynamic-table'),
+        Number(localStorage.getItem('tableColumnsNumber')),
+        Number(localStorage.getItem('tableRowsNumber'))
+    ));
